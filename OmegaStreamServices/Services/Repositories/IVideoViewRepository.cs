1b8e72155ce9046e1ad01c54f742bf3c0a39e17b
Pitius <ballaipeterlorant@gmail.com>
2025-03-06 00:04:31 +0100

    Added VideoViewsRepository to get ViewHistory for a User

---
+++ OmegaStreamServices/Services/Repositories/IVideoViewRepository.cs
@@ -0,0 +1,15 @@
+﻿using OmegaStreamServices.Models;
+using OmegaStreamServices.Services.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaStreamServices.Services.Repositories
+{
+    public interface IVideoViewRepository: IBaseRepository<VideoView>
+    {
+        Task<List<VideoView>> GetUserViewHistory(string userId);
+    }
+}
---
+++ OmegaStreamServices/Services/Repositories/VideoViewRepository.cs
@@ -0,0 +1,25 @@
+﻿using OmegaStreamServices.Data;
+using OmegaStreamServices.Models;
+using OmegaStreamServices.Services.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaStreamServices.Services.Repositories
+{
+    public class VideoViewRepository : BaseRepository<VideoView>, IVideoViewRepository
+    {
+        public VideoViewRepository(AppDbContext context) : base(context)
+        {
+            
+        }
+        public async Task<List<VideoView>> GetUserViewHistory(string userId)
+        {
+            var videos = await _dbSet.Where(x => x.UserId == userId).ToListAsync();
+            return videos;
+        }
+    }
+}
