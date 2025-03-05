8cbd5a35fb76f16f88b71b749b26ef4836ccd728
Pitius <ballaipeterlorant@gmail.com>
2025-03-05 22:37:08 +0100

    Added VideoView Model

---
+++ OmegaStreamServices/Models/VideoView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaStreamServices.Models
+{
+    public class VideoView
+    {
+        public string? UserId { get; set; }
+        public virtual User? User { get; set; }
+
+        [NotMapped]
+        public string? IpAddress { get; set; }
+
+        public DateTime ViewedAt { get; set; } = DateTime.UtcNow;
+
+        public int VideoId { get; set; }
+        public virtual Video Video { get; set; }
+    }
+}
