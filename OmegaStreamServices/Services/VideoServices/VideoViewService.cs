04ed5af2d2343fa9a4aa184d1b88b7754b549de5
Pitius <ballaipeterlorant@gmail.com>
2025-03-06 00:52:06 +0100

    Added VideoView Service

---
+++ OmegaStreamServices/Services/VideoServices/VideoViewService.cs
@@ -0,0 +1,47 @@
+﻿using OmegaStreamServices.Models;
+using OmegaStreamServices.Services.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaStreamServices.Services.VideoServices
+{
+    public class VideoViewService : IVideoViewService
+    {
+        private readonly int _viewCooldownSeconds = 30;
+        private readonly IVideoViewRepository _videoViewRepository;
+        private readonly IVideoRepository _videoRepository;
+
+        public VideoViewService(IVideoViewRepository videoViewRepository)
+        {
+            _videoViewRepository = videoViewRepository;
+        }
+        public async Task ValidateView(VideoView view)
+        {
+            if (view.UserId == null) {
+                //TODO: Guest
+            }
+            else
+            {
+                if(ToUnixMillis(DateTime.UtcNow) - ToUnixMillis(view.ViewedAt) > _viewCooldownSeconds*1000)
+                {
+                    _videoViewRepository.Add(view);
+                }
+                
+            }
+        }
+
+        private long ToUnixMillis(DateTime dateTime)
+        {
+            DateTime utcDateTime = dateTime.ToUniversalTime();
+
+            DateTimeOffset epoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+            long unixMilliseconds = (long)(utcDateTime - epoch).TotalMilliseconds;
+
+            return unixMilliseconds;
+        }
+    }
+}
