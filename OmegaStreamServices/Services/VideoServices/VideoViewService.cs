b385a925396df0d5610412429ecc65736de2c82a
Ballai Péter <ballaipeterlorant@gmail.com>
2025-03-06 13:31:18 +0100

    Fixed ValidateView not updating videos table

--- OmegaStreamServices/Services/VideoServices/VideoViewService.cs
+++ OmegaStreamServices/Services/VideoServices/VideoViewService.cs
@@ -1,4 +1,5 @@
-﻿using OmegaStreamServices.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using OmegaStreamServices.Models;
 using OmegaStreamServices.Services.Repositories;
 using System;
 using System.Collections.Generic;
@@ -13,27 +14,34 @@ namespace OmegaStreamServices.Services.VideoServices
 
         private readonly IVideoViewRepository _videoViewRepository;
         private readonly IVideoRepository _videoRepository;
+        private readonly UserManager<User> _userManager;
 
-        public VideoViewService(IVideoViewRepository videoViewRepository)
+        public VideoViewService(IVideoViewRepository videoViewRepository, IVideoRepository videoRepository, UserManager<User> userManager)
         {
             _videoViewRepository = videoViewRepository;
+            _videoRepository = videoRepository;
+            _userManager = userManager;
         }
         public async Task ValidateView(VideoView view)
         {
+            view.Video = await _videoRepository.GetVideoWithInclude(view.VideoId);
             if (view.UserId == null) {
                 _videoViewRepository.RemoveOutdatedGuestViews();
                 if (!VideoViewRepository.GuestViews.Any(v => v.IpAddressHash == view.IpAddressHash))
                 {
                     _videoViewRepository.AddGuestView(view);
                     view.Video.Views++;
+                    _videoRepository.Update(view.Video);
                 }
             }
             else
             {
+                view.User = await _userManager.FindByIdAsync(view.UserId);
                 if(ToUnixMillis(DateTime.UtcNow) - ToUnixMillis(view.ViewedAt) > VideoViewRepository.ViewCooldown*1000)
                 {
                     await _videoViewRepository.Add(view);
                     view.Video.Views++;
+                    _videoRepository.Update(view.Video);
                 }
                 
             }
