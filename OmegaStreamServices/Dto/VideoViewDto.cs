063a38600230d3f8ea50fc77fe63feee34ba0ab6
Pitius <ballaipeterlorant@gmail.com>
2025-03-07 20:24:34 +0100

    Added VideoViewDto for view history

---
+++ OmegaStreamServices/Dto/VideoViewDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaStreamServices.Dto
+{
+    public class VideoViewDto
+    {
+        public string UserId { get; set; }
+        public UserDto User { get; set; }
+        public DateTime ViewedAt { get; set; } = DateTime.UtcNow;
+        public int VideoId { get; set; }
+        public VideoDto Video { get; set; }
+    }
+}
