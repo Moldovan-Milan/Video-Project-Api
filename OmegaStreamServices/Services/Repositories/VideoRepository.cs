ab9895fcefb2b196891f1b5c17be6fe926dd258d
Moldován Milán János <moldovanmilan2005@gmail.com>
2025-03-17 20:18:03 +0100

    Updated sorting

--- OmegaStreamServices/Services/Repositories/VideoRepository.cs
+++ OmegaStreamServices/Services/Repositories/VideoRepository.cs
@@ -5,91 +5,83 @@ using OmegaStreamServices.Services.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace OmegaStreamServices.Services.Repositories
 {
     public class VideoRepository : BaseRepository<Video>, IVideoRepository
     {
+        private readonly Random random;
+
         public VideoRepository(AppDbContext context) : base(context)
         {
+            random = new Random();
         }
 
         public async Task<List<Video>> GetAllVideosWithIncludes(int pageNumber, int pageSize)
         {
-            var videos = await _dbSet
+            return await Task.FromResult(_dbSet
                 .Include(v => v.User)
                 .Include(v => v.Thumbnail)
-                .Include(x => x.Comments)
-                .Include(x => x.VideoLikes).ToListAsync();
-            videos = await SortVideos(videos);
-
-            return videos.Skip((pageNumber - 1) * pageSize)
-                .Take(pageSize).ToList();
+                .Include(v => v.Comments)
+                .Include(v => v.VideoLikes)
+                .AsEnumerable() // Exit from sql query
+                .OrderByDescending(v => CalculateScore(
+                    v.Views,
+                    v.VideoLikes.Count(l => !l.IsDislike),
+                    v.VideoLikes.Count(l => l.IsDislike),
+                    v.Comments.Count,
+                    (DateTime.UtcNow - v.Created).TotalDays,
+                    (DateTime.UtcNow - v.Created).TotalHours))
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList());
         }
 
         public async Task<List<Video>> GetVideosByName(string name, int pageNumber, int pageSize)
         {
             name = name.ToLower();
-            var videos = await _dbSet
-                .Where(x => x.Title.ToLower().Contains(name))
-                .Include(x => x.User)
-                .Include(x => x.Thumbnail)
-                .Include(x => x.Comments)
-                .Include(x => x.VideoLikes).ToListAsync();
-
-            videos = await SortVideos(videos);
-
-            return videos.Skip((pageNumber - 1) * pageSize)
-                .Take(pageSize).ToList();
+            return await Task.FromResult(_dbSet
+                .Where(v => v.Title.ToLower().Contains(name))
+                .Include(v => v.User)
+                .Include(v => v.Thumbnail)
+                .Include(v => v.Comments)
+                .Include(v => v.VideoLikes)
+                .AsEnumerable() // Exit from sql query
+                .OrderByDescending(v => CalculateScore(
+                    v.Views,
+                    v.VideoLikes.Count(l => !l.IsDislike),
+                    v.VideoLikes.Count(l => l.IsDislike),
+                    v.Comments.Count,
+                    (DateTime.UtcNow - v.Created).TotalDays,
+                    (DateTime.UtcNow - v.Created).TotalHours))
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList());
         }
 
-        public async Task<Video> GetVideoWithInclude(int id)
+        public async Task<Video?> GetVideoWithInclude(int id)
         {
-            Video video = await _dbSet.Include(v => v.User).Include(v => v.Thumbnail)
-                .Include(x => x.Comments).ThenInclude(x => x.User)
-                .FirstOrDefaultAsync(v => v.Id == id)!;
-            return video;
+            return await _dbSet
+                .Include(v => v.User)
+                .Include(v => v.Thumbnail)
+                .Include(v => v.Comments)
+                    .ThenInclude(c => c.User)
+                .FirstOrDefaultAsync(v => v.Id == id);
         }
 
-        public Task<List<Video>> SortVideos(List<Video> videos)
-        {
-            var sortedVideos = videos
-                .Select(v => new
-                {
-                    Video = v,
-                    Likes = v.VideoLikes.Count(l => !l.IsDislike),
-                    Dislikes = v.VideoLikes.Count(l => l.IsDislike),
-                    Views = v.Views,
-                    Comments = v.Comments.Count,
-                    Days = Math.Max((DateTime.UtcNow.Date - v.Created).TotalDays, 0)
-                })
-                .OrderByDescending(v => CalculateScore(v.Views, v.Likes, v.Dislikes, v.Comments, v.Days))
-                .Select(v => v.Video)
-                .ToList();
-
-            return Task.FromResult(sortedVideos);
-        }
-
-
-        /// <summary>
-        /// Calculate video score based on views, likes, dislikes, comments, and time.
-        /// </summary>
-        /// <param name="V">Number of views</param>
-        /// <param name="L">Number of likes</param>
-        /// <param name="D">Number of dislikes</param>
-        /// <param name="C">Number of comments</param>
-        /// <param name="time">Age of the video in days</param>
-        /// <returns>Calculated score</returns>
-
-        private double CalculateScore(long V, int L, int D, int C, double time)
+        private double CalculateScore(long V, int L, int D, int C, double time, double hours)
         {
             double safeTime = Math.Max(time, 1);
-            return ((V + 100) * 1.5 + L * 2 - D * 2 + C * 0.5)
-                  / (2 + Math.Pow(safeTime, 0.2));
-            //return (L - D / V + 1) + C / 10 + Math.Exp(-safeTime / 30.0);
-        }
+            double score = ((V + 100) * 1.5 + L * 2 - D * 2 + C * 0.5)
+                           / (2 + Math.Pow(safeTime, 0.2));
 
+            if (hours < 1)
+                score *= 2;
+            if (random.Next(0, 99) == 0) // 1% chance to increase the points by 10x
+                score *= 10;
+
+            return score;
+        }
     }
 }
