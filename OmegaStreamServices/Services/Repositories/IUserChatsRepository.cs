3e91ed904b2dc5545cb6f48e0ea9a8df4c517bf1
Pitius <ballaipeterlorant@gmail.com>
2025-03-24 22:52:23 +0100

    Added DeleteAllMessages Method to UserChatRepository

--- OmegaStreamServices/Services/Repositories/IUserChatsRepository.cs
+++ OmegaStreamServices/Services/Repositories/IUserChatsRepository.cs
@@ -1,15 +1,11 @@
 ﻿using OmegaStreamServices.Models;
 using OmegaStreamServices.Services.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OmegaStreamServices.Services.Repositories
 {
-    public interface IUserChatsRepository: IBaseRepository<UserChats>
+    public interface IUserChatsRepository : IBaseRepository<UserChats>
     {
         Task<List<UserChats>> GetAllChatByUserIdAsync(string userId);
+        Task DeleteAllMessages(int chatId);
     }
 }
--- OmegaStreamServices/Services/Repositories/UserChatsRepository.cs
+++ OmegaStreamServices/Services/Repositories/UserChatsRepository.cs
@@ -2,11 +2,6 @@
 using OmegaStreamServices.Data;
 using OmegaStreamServices.Models;
 using OmegaStreamServices.Services.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OmegaStreamServices.Services.Repositories
 {
@@ -18,9 +13,16 @@ namespace OmegaStreamServices.Services.Repositories
 
         public async Task<List<UserChats>> GetAllChatByUserIdAsync(string userId)
         {
-            return await _dbSet.Include(x => x.User1).Include(x => x.User2) 
+            return await _dbSet.Include(x => x.User1).Include(x => x.User2)
                 .Where(x => x.User1Id == userId || x.User2Id == userId)
                 .ToListAsync();
+        }
+
+        public async Task DeleteAllMessages(int chatId)
+        {
+            var messages = await _context.ChatMessages.Where(x => x.UserChatId == chatId).ToListAsync();
+            _context.ChatMessages.RemoveRange(messages);
+            await _context.SaveChangesAsync();
         }
     }
 }
