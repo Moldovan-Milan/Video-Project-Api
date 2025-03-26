c29e36176b4c1ae1a0ea47a8c7ae0bcf339bb724
Pitius <ballaipeterlorant@gmail.com>
2025-03-26 21:13:56 +0100

    Added admin controller with admin-test endpoint

---
+++ OmegaStreamWebAPI/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OmegaStreamWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet("admin-test")]
+        public IActionResult AdminTest()
+        {
+            return Ok("If you see this, you are and admin user!");
+        }
+    }
+}
