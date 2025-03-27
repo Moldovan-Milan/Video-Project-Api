d4717c913be567503e176c92369505e833c84fcb
Ballai Péter <ballaipeterlorant@gmail.com>
2025-03-27 11:36:26 +0100

    Made admins be able to delete any user

--- OmegaStreamWebAPI/Controllers/AdminController.cs
+++ OmegaStreamWebAPI/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OmegaStreamServices.Models;
 
 namespace OmegaStreamWebAPI.Controllers
 {
@@ -8,10 +10,31 @@ namespace OmegaStreamWebAPI.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : ControllerBase
     {
+        private UserManager<User> _userManager;
+        private UserService _userService;
+
+        public AdminController(UserManager<User> userManager, UserService userService)
+        {
+            _userManager = userManager;
+            _userService = userService;
+        }
+
         [HttpGet("admin-test")]
         public IActionResult AdminTest()
         {
             return Ok("If you see this, you are an admin user!");
+        }
+
+        [HttpDelete("delete-user/{userId}")]
+        public async Task<IActionResult> Delete([FromRoute] string userId) 
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            await _userService.DeleteAccount(userId);
+            return NoContent();
         }
     }
 }
