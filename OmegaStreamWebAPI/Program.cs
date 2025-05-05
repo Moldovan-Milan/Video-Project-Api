fa6a24ffd1104ec8646aec4f778c9b1d3ffe0a4b
Moldován Milán János <moldovanmilan2005@gmail.com>
2025-05-05 14:46:10 +0200

    Enhance password security requirements
    Updated `IdentityOptions` to enforce stronger password policies, including mandatory digits, lowercase and uppercase letters, non-alphanumeric characters, a minimum length of 8 characters, and at least 1 unique character. Removed previous weak password settings.

--- OmegaStreamWebAPI/Program.cs
+++ OmegaStreamWebAPI/Program.cs
@@ -78,12 +78,12 @@ namespace OmegaStreamWebAPI
                 {
                     // Majd az éles verzióban lesz engedélyezve
                     // Password settings.
-                    options.Password.RequireDigit = false;
-                    options.Password.RequireLowercase = false;
-                    options.Password.RequireNonAlphanumeric = false;
-                    options.Password.RequireUppercase = false;
-                    options.Password.RequiredLength = 3;
-                    options.Password.RequiredUniqueChars = 0;
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireNonAlphanumeric = true;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequiredLength = 8;
+                    options.Password.RequiredUniqueChars = 1;
 
                     // Lockout settings.
                     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
